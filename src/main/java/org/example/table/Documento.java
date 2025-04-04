32e3b81f2c6f381e8cd747583f215483b1111924
Samuel Mendes <samuelmendespy@gmail.com>
2025-04-03 22:17:49 -0300

    feat(documento): criar classe Documento com enum TipoDocumento

---
+++ src/main/java/org/example/table/Documento.java
@@ -0,0 +1,37 @@
+package org.example.table;
+
+public class Documento {
+    private TipoDocumento tipo;
+    private String numero;
+
+    public Documento(TipoDocumento tipo, String numero) {
+        this.tipo = tipo;
+        this.numero = numero;
+    }
+
+    public TipoDocumento getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoDocumento tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Documento{" +
+                "tipo=" + tipo.getSigla() +
+                ", numero=" + numero +
+                ", descricao='" + tipo.getDescricao() + '\'' +
+                '}';
+    }
+}
+
